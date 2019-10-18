f4e35e35bd3a49edee82d522384bfdc4dc4c3195
Ernst Bunders <ernst.bunders@topicus.nl>
2019-10-18 08:49:08 +0200

    add_watermerk geupdate voor gebruik met nautilus-scripts

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # scripts
+
+## nautilus scripts
+Wanneer je een script wilt gebruiken vanuit nautilus (rechts klikken -> scripts),
+dien je het te plaatsen in de map:
+
+`~/.local/share/nautilus/scripts/`
+
+Er zijn twee dependencies:
+- python3
+- imagemagick (convert)
+
+Nautilus injecteerd de volgende variabelen:
+- NAUTILUS_SCRIPT_SELECTED_FILE_PATHS: newline-delimited paths for selected files (only if local)
+- NAUTILUS_SCRIPT_SELECTED_URIS: newline-delimited URIs for selected files
+- NAUTILUS_SCRIPT_CURRENT_URI: current location
+- NAUTILUS_SCRIPT_WINDOW_GEOMETRY position and size of current window 
+
+referentie:
+- https://fedoramagazine.org/integrating-scripts-nautilus/
+
+## add_watermark
+Dit script voegt aan plaatjes een watermerk toe: www.baukjemaakthet.nl
+
--- add-watermark.sh
+++ add-watermark.sh
@@ -2,27 +2,28 @@
 
 import sys
 from subprocess import call
+import os
 
-# print 'Number of arguments:', len(sys.argv), 'arguments.'
-# print 'Argument List:', str(sys.argv)
-inFile = sys.argv[1]
-lastDot=inFile.rfind(".")
-baseName = inFile[0:lastDot]
-extension = inFile[lastDot:]
-outFile = baseName + "_watermerk" + extension
-watermark="www.baukjemaakthet.nl"
+def convertImage(inFile):
+    lastDot=inFile.rfind(".")
+    baseName = inFile[0:lastDot]
+    extension = inFile[lastDot:]
+    outFile = baseName + "_watermerk" + extension
+    watermark="www.baukjemaakthet.nl"
 
-cmd = ("convert \"" + inFile + "\" "
-" -gravity southwest "
-" -strokewidth 2 -stroke '#000C' "
-" -pointsize 20 "
-" -draw 'text 25,25 \""+watermark+"\"' "
-" -stroke none -fill white "
-" -draw 'text 25,25 \""+watermark+"\"' \"" + outFile + "\"")
+    cmd = ("convert \"" + inFile + "\" "
+    " -gravity southwest "
+    " -strokewidth 2 -stroke '#000C' "
+    " -pointsize 20 "
+    " -draw 'text 25,25 \""+watermark+"\"' "
+    " -stroke none -fill white "
+    " -draw 'text 25,25 \""+watermark+"\"' \"" + outFile + "\"")
 
-# print cmd
+    call([cmd], shell=True
+    , stdout = sys.stdout
+    , stderr = sys.stderr
+    )
 
-call([cmd], shell=True
-, stdout = sys.stdout
-, stderr = sys.stderr
-)
+selectedFiles = os.environ["NAUTILUS_SCRIPT_SELECTED_FILE_PATHS"]
+
+[convertImage(file) for file in selectedFiles.splitlines()]
